24f86693740346528b47207409e9a76c2a949629
Taisia Mertz <tmertz@usc.edu>
2023-08-11 19:04:43 -0400

    fixed about me section

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -196,13 +196,11 @@ class MainPage extends React.Component {
                                 </button>
                             </div>
                         </div>
-                        {/* <img className='techBackground' src={techBackground} alt="Tech Background" />
-                        <img className='headshot' src={headshot} alt="Headshot" /> */}
                         <img className='background' src={background} alt="background"/>
                     </section>
                     <section ref={this.aboutMeRef} id="aboutSec" className="aboutSec">
-                        <label className="aboutMeTitle">ABOUT ME</label>
                         <div className="aboutSecInner">
+                        <label className="aboutMeTitle">ABOUT ME</label>
                             <p className="aboutMeInfo">I am a highly motivated student at the University of Southern 
                             California's Viterbi School of Engineering, pursuing a major in Computer Science with a 
                             strong background in general engineering.</p>
@@ -218,36 +216,36 @@ class MainPage extends React.Component {
                             My ultimate goal is to leverage technology to tackle real-world challenges and make a 
                             positive impact. I am committed to utilizing my skills and knowledge to develop innovative 
                             solutions that address these challenges head-on.</p>
-                            
-                        </div>
-                        <div className="contactOptions">
+                            <div className="contactOptions">
                                 <a href="https://github.com/mertz-taisia" target="_blank">
                                     <button class="githubContactButton">
-                                        <svg fill='white' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" width="35px" height="35px" class="github">    <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path></svg>
+                                        <svg fill='white' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30" className = "iconSizeGithub">    <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path></svg>
                                         <span class="githubContactLabel">@mertz-taisia</span>
                                     </button>
                                 </a>
                                 <button class="emailContactButton" onClick={handleEmailClick}>
-                                    <svg fill="white" viewBox="0 0 16 16" height="2.05em" width="1.65em" xmlns="http://www.w3.org/2000/svg">
+                                    <svg fill="white" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" className = "iconSize">
                                         <path d="M14 2H2c-1.1 0-1.99.9-1.99 2L0 12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 4l-6 3.5L2 6V4l6 3.5L14 4v2z"/>
                                     </svg>
                                     <span class="emailContactLabel">tmertz@usc.edu</span>
                                 </button>
                                 <a href="https://www.linkedin.com/in/taisia-mertz-6a3782224/" target="_blank">
                                     <button class="linkedInContactButton">
-                                        <svg fill="white" viewBox="0 0 24 24" height="1.5em" width="1.5em" xmlns="http://www.w3.org/2000/svg"><path d="M22.23 0H1.77C.79 0 0 .79 0 1.77v20.46C0 23.21.79 24 1.77 24h20.46c.99 0 1.77-.79 1.77-1.77V1.77C24 .79 23.21 0 22.23 0zM7.53 20.48H3.56V9h3.97v11.48zM5.55 7.74a2.26 2.26 0 0 1-2.26-2.26 2.26 2.26 0 0 1 2.26-2.26c1.24 0 2.25 1.01 2.25 2.26a2.26 2.26 0 0 1-2.25 2.26zm15.25 12.74h-3.96v-5.57c0-1.33 0-3.06-1.86-3.06-1.87 0-2.15 1.46-2.15 2.96v5.67H9.88V9h3.81v1.56h.05c.53-.99 1.82-2.05 3.75-2.05 4.01 0 4.76 2.64 4.76 6.08v5.89z"></path></svg>
+                                        <svg fill="white" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg" className = "iconSize"><path d="M22.23 0H1.77C.79 0 0 .79 0 1.77v20.46C0 23.21.79 24 1.77 24h20.46c.99 0 1.77-.79 1.77-1.77V1.77C24 .79 23.21 0 22.23 0zM7.53 20.48H3.56V9h3.97v11.48zM5.55 7.74a2.26 2.26 0 0 1-2.26-2.26 2.26 2.26 0 0 1 2.26-2.26c1.24 0 2.25 1.01 2.25 2.26a2.26 2.26 0 0 1-2.25 2.26zm15.25 12.74h-3.96v-5.57c0-1.33 0-3.06-1.86-3.06-1.87 0-2.15 1.46-2.15 2.96v5.67H9.88V9h3.81v1.56h.05c.53-.99 1.82-2.05 3.75-2.05 4.01 0 4.76 2.64 4.76 6.08v5.89z"></path></svg>
                                         <span class="linkedInContactLabel">@taisia mertz</span>
                                     </button>
                                 </a>
                                 <a href={resume} target="_blank" rel="noreferrer noopener">
                                     <button class="resumeContactButton">
-                                        <svg fill="white" viewBox="0 0 25 25" height="2em" width="2em" xmlns="http://www.w3.org/2000/svg">
+                                        <svg fill="white" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg" className = "iconSize">
                                             <path d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z"/>
                                         </svg>
                                         <span class="resumeContactLabel">Taisia Mertz Resume.pdf</span>
                                     </button>
                                 </a>
                             </div>
+                        </div>
+                        
                     </section>
                     <section className='gap'></section>
                     <section ref={this.experienceRef} id="experienceSec" className="experienceSec">
