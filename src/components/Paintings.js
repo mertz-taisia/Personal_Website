338551ca7cd38fb1b9d29e4989489c6081c256fa
Taisia Mertz <tmertz@usc.edu>
2023-08-14 04:30:57 -0400

    created gallary layout and input paintings page images

--- src/components/Paintings.js
+++ src/components/Paintings.js
@@ -2,6 +2,19 @@ import React from 'react'
 import { Helmet } from 'react-helmet';
 import './Paintings.css'
 import { Link } from 'react-router-dom';
+import parisMagnolias from '../images/paintings/paris_magnolias.jpg';
+import santorini from '../images/paintings/santorini.jpg';
+import londonRain from '../images/paintings/london_rain.jpg';
+import cityStorm from '../images/paintings/city_storm.jpg';
+import wave from '../images/paintings/wave.jpg';
+import eveningSky from '../images/paintings/evening_sky.jpg';
+import lightHouse from '../images/paintings/light_house.jpg';
+import castle from '../images/paintings/castle.jpg';
+import bouquet from '../images/paintings/bouquet.jpg';
+import crocodile from '../images/paintings/crocodile.jpg';
+import telephoneWires from '../images/paintings/telephone_wires.jpg';
+import flowerTri from '../images/paintings/flower_tri.jpg';
+
 
 function Paintings() {
   return (
@@ -22,11 +35,100 @@ function Paintings() {
             </div>
         <div className="jobDivider"></div>
         <div className='pltwPageInner'>
-            <div className='jobInfo'>
-                <label className='jobPosition'>PAINTING ACROSS VARIOUS MEDIUMS</label>
-                <label className='jobPageDate'>Present</label>
+            <div className='paintingContent'>
+                <div className='paintingsLeft'>
+                    <div className='paintingCard'>
+                        <div className='paintingCardInner'>
+                            <img src = {parisMagnolias} alt = "error"></img>
+                            <label className='paintingCardTitle'>Layover in Paris</label>
+                            <label className='paintingCardMedium'>Acrylic on Wood</label>
+                        </div>
+                    </div>
+                    <div className='paintingCard'>
+                        <div className='paintingCardInner'>
+                            <img src = {wave} alt = "error"></img>
+                            <label className='paintingCardTitle'>Sea Foam</label>
+                            <label className='paintingCardMedium'>Acrylic on Paper</label>
+                        </div>
+                    </div>
+                </div>
+                <div className='paintingsCenterLeft'>
+                    <div className='paintingCard'>
+                        <div className='paintingCardInner'>
+                            <img src = {santorini} alt = "error"></img>
+                            <label className='paintingCardTitle'>Caldera's Embrace</label>
+                            <label className='paintingCardMedium'>Oil of Canvas</label>
+                        </div>
+                    </div>
+                    <div className='paintingCard'>
+                        <div className='paintingCardInner'>
+                            <img src = {eveningSky} alt = "error"></img>
+                            <label className='paintingCardTitle'>Evening Sky</label>
+                            <label className='paintingCardMedium'>Acrylic on Paper</label>
+                        </div>
+                    </div>
+                    <div className='paintingCard'>
+                        <div className='paintingCardInner'>
+                            <img src = {bouquet} alt = "error"></img>
+                            <label className='paintingCardTitle'>Un Petit Bouquet</label>
+                            <label className='paintingCardMedium'>Acrylic on Canvas</label>
+                        </div>
+                    </div>
+                </div>
+                <div className='paintingsCenterRight'>
+                    <div className='paintingCard'>
+                        <div className='paintingCardInner'>
+                            <img src = {londonRain} alt = "error"></img>
+                            <label className='paintingCardTitle'>London Rain</label>
+                            <label className='paintingCardMedium'>Acrylic on Paper</label>
+                        </div>
+                    </div>
+                    <div className='paintingCard'>
+                        <div className='paintingCardInner'>
+                            <img src = {lightHouse} alt = "error"></img>
+                            <label className='paintingCardTitle'>Lighthouse</label>
+                            <label className='paintingCardMedium'>Acrylic on Paper</label>
+                        </div>
+                    </div>
+                    <div className='paintingCard'>
+                        <div className='paintingCardInner'>
+                            <img src = {crocodile} alt = "error"></img>
+                            <label className='paintingCardTitle'>Lighthouse</label>
+                            <label className='paintingCardMedium'>Acrylic on Paper</label>
+                        </div>
+                    </div>
+                    <div className='paintingCard'>
+                        <div className='paintingCardInner'>
+                            <img src = {telephoneWires} alt = "error"></img>
+                            <label className='paintingCardTitle'>Telephone Wires</label>
+                            <label className='paintingCardMedium'>Acrylic on Paper</label>
+                        </div>
+                    </div>
+                </div>
+                <div className='paintingsRight'>
+                    <div className='paintingCard'>
+                        <div className='paintingCardInner'>
+                            <img src = {cityStorm} alt = "error"></img>
+                            <label className='paintingCardTitle'>Stormy Nights</label>
+                            <label className='paintingCardMedium'>Acrylic on Paper</label>
+                        </div>
+                    </div>
+                    <div className='paintingCard'>
+                        <div className='paintingCardInner'>
+                            <img src = {castle} alt = "error"></img>
+                            <label className='paintingCardTitle'>French Castle</label>
+                            <label className='paintingCardMedium'>Oil on Canvas</label>
+                        </div>
+                    </div>
+                    <div className='paintingCard'>
+                        <div className='paintingCardInner'>
+                            <img src = {flowerTri} alt = "error"></img>
+                            <label className='paintingCardTitle'>Flower Tri</label>
+                            <label className='paintingCardMedium'>Acrylic on Paper</label>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className='paintingContent'>Insert paintings</div>
         </div>
     </div>
   )
